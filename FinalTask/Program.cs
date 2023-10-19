d512233cdea125f6662f36281dac42490d77cf59
Aleksey Blinov <blinov71111@gmail.com>
2023-10-19 20:47:59 +0300

    Добавил код в папку FinalTask

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
+string[] array1 = new string[] {"Russia", "Denmark", "Kazan" };
+
+void PrintData(string[] stringArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.Write($"{stringArray[i]}, ");
+    }
+    Console.Write("]");
+}
+
+string[] ArryaNew(string[] aray)
+{
+    int count = 0;
+// Перебираем массив и проверяем условие
+    for (int i = 0; i < aray.Length; i++)
+    {
+        if (aray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+// Собираем новый массив
+    string[] arrya2 = new string[count];
+    int count2 = 0;
+    for (int i = 0; i < aray.Length; i++)
+    {
+        if (aray[i].Length <= 3)
+        {
+            arrya2[count2] = aray[i];
+            count2++;
+
+        }
+    }
+
+    return arrya2;
+}
+
+// Результат метода
+string[] errayNew = ArryaNew(array1);
+PrintData(array1);
+Console.Write(" -> ");
+PrintData(errayNew);
